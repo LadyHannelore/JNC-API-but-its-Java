7936a2403b559c6b428902074ba1460580a79a60
umerzaidi <63396185+umerzaidi@users.noreply.github.com>
2023-07-18 14:54:38 +0500

    first commit

---
+++ web.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class web {
+   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Scanner filein;
+    PrintWriter fileout;
+    String filenamein;
+    String filenameout;
+    
+    
+   } 
+    
+}
